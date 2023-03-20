f3e9873301deb9218feca7f55fad90b78cfdc4f2
Adriana Davi <56098105+Megadrity@users.noreply.github.com>
2023-03-20 16:28:05 -0300

    Update app1.js

--- AtividadeWeb2/Js/app1.js
+++ AtividadeWeb2/Js/app1.js
@@ -1,22 +1,48 @@
 const entrada = document.querySelector('#entrada');
 const btn = document.querySelector('#btn');
+const remove = document.getAttribute('#remove');
 const lista = document.querySelector('#lista');
-btn.onclick = () => {
-    if(entrada.value != ''){
+removeAttributeNode('#remove');
+btn.onclick = () =>{}
+    if (entrada.value != ''){
+      alertify.confirm(
+        "Confirmação de cadastro",
+        "Você tem certeza que quer incluir o item " + entrada.value,
+      );
+      function(){ 
+
         let itemLista = document.createElement('li');
         let texto = document.createTextNode(entrada.value);
-        let iconeOk = document.createElement('ion-icon');
+        let icone = document.createElement('ion-icon');
         let iconeRemover = document.createElement('ion-icon');
-        iconeOk.classList.add('icone');
-        iconeOk.setAttribute('name', 'checkmark-circle-outline');
+        let remove = document.getElementById('remove');
+        icone.classList.add('icone');
+        icone.setAttribute('name', 'checkmark-circle-outline');
         iconeRemover.classList.add('icone2');
         iconeRemover.setAttribute('name', 'close-circle-outline');
         itemLista.append(texto);
-        itemLista.append(iconeOk);
+        itemLista.append(icone);
         itemLista.append(iconeRemover);
-        console.log(itemLista);
-        lista.append(lista);
-    }else{
-        alert('Você precisa preencher o campo');
-    }
- };
+        lista.append(itemLista);
+        alertify.success("Item adicionado.");
+        entrada.value = ""; 
+      }
+        function(){
+          alertify.error('Não inserido');
+          entrada.value = "";
+        else{
+          alertify.alert('Por favor preencher o campo');
+        }
+      }
+      function removerForm(){ 
+        document.getElementById("div").lastChild.remove();
+    
+    
+     if (remove.parentNode){
+       remove.parentNode.removeChild(remove);
+    
+      } 
+  }
+  
+  
+};
